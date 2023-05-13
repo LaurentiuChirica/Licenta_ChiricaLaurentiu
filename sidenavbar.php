cc364998cd5481a0f7641f699c1a795cf1e2ae36
LaurentiuChirica <laurentiu.gabriel.chirica@gmail.com>
2023-05-13 16:50:13 +0300

    Another change

---
+++ sidenavbar.php
@@ -0,0 +1,107 @@
+<link rel="stylesheet" href="sidenavbar.css" type="text/css">
+
+<nav class="navigation">
+    <ul class="mainmenu">
+        <?php
+        $menu_items = [
+            [
+                'label' => 'Home',
+                'url' => '/'
+            ],
+            [
+                'label' => 'Products',
+                'url' => '#',
+                'sub_menu' => [
+                    [
+                        'label' => 'Product 1',
+                        'url' => '#'
+                    ],
+                    [
+                        'label' => 'Product 2',
+                        'url' => '#'
+                    ],
+                    [
+                        'label' => 'Product 3',
+                        'url' => '#'
+                    ]
+                ]
+            ],
+            [
+                'label' => 'Services',
+                'url' => '#',
+                'sub_menu' => [
+                    [
+                        'label' => 'Service 1',
+                        'url' => '#'
+                    ],
+                    [
+                        'label' => 'Service 2',
+                        'url' => '#'
+                    ],
+                    [
+                        'label' => 'Service 3',
+                        'url' => '#'
+                    ]
+                ]
+            ],
+            [
+                'label' => 'Contact',
+                'url' => '/contact'
+            ],
+            [
+                'label' => 'Contact',
+                'url' => '/contact'
+            ],
+            [
+                'label' => 'Contact',
+                'url' => '/contact'
+            ],
+            [
+                'label' => 'Contact',
+                'url' => '/contact'
+            ],
+            [
+                'label' => 'Contact',
+                'url' => '/contact'
+            ],
+            [
+                'label' => 'Contact',
+                'url' => '/contact'
+            ],
+            [
+                'label' => 'Contact',
+                'url' => '/contact'
+            ],
+            [
+                'label' => 'Contact',
+                'url' => '/contact'
+            ],
+            [
+                'label' => 'Contact',
+                'url' => '/contact'
+            ],
+            [
+                'label' => 'Contact',
+                'url' => '/contact'
+            ],
+            [
+                'label' => 'Contact',
+                'url' => '/contact'
+            ],
+        ];
+
+        foreach ($menu_items as $item) {
+            echo '<li>';
+            echo '<a class href="' . $item['url'] . '">' . $item['label'] . '</a>';
+            if (isset($item['sub_menu'])) {
+                echo '<ul class="submenu">';
+                foreach ($item['sub_menu'] as $sub_item) {
+                    echo '<li><a "href="' . $sub_item['url'] . '">' . $sub_item['label'] . '</a></li>';
+                }
+                echo '</ul>';
+            }
+            echo '</li>';
+        }
+        ?>
+    </ul>
+</nav>
